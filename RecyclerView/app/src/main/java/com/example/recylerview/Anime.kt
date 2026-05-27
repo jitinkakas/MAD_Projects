279ae43e5b1ea3dc98bc19b37f10a8aa1df16463
Jitin Yadav <Jitinyadav9898@gmail.com>
2026-05-28 01:02:43 +0530

    Third commit

---
+++ RecyclerView/app/src/main/java/com/example/recylerview/Anime.kt
@@ -0,0 +1,17 @@
+package com.example.recylerview
+
+import androidx.appcompat.widget.DialogTitle
+
+class Anime {
+
+    var title : String
+    var name : String
+    var image : Int
+
+    constructor(title : String, name : String, img : Int){
+        this.title = title
+        this.name = name
+        this.image = img
+    }
+
+}
